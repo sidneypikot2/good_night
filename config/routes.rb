26d654229aff390e0f35cf1d5326afd98f3b1653
Sidney John Diongzon <sidney@metromart.com>
2023-06-24 12:13:53 +0800

    Create Users - CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  scope "/api", defaults: {format: :json} do
+    resources :users
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
