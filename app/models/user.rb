931570113d57560954d1408058d0e25541bc1060
Sidney John Diongzon <sidney@metromart.com>
2023-06-25 16:22:08 +0800

    Update: remove Follow on User#destroy via followed_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,10 @@
 class User < ApplicationRecord
   has_many :follows, dependent: :destroy
+  has_many :followers, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy
   has_many :following, through: :follows
   has_many :clock_ins, dependent: :destroy
   has_many :follow_clock_ins, through: :follows
-  has_one  :current_clock_in, -> { current_clock_in }, class_name: "ClockIn", inverse_of: :user
+  has_one  :current_clock_in, -> { current_clock_in }, class_name: 'ClockIn', inverse_of: :user
 
   validates_presence_of :name
-
-  def following_clock_ins
-    includes(follows: :clock_ins).clock_ins.where
-  end
 end
